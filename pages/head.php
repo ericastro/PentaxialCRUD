d3ebbaec9c451431bb777c619f507002621f42ec
Eric Oliveira de Castro <eric.castro.dev@gmail.com>
2019-08-29 18:05:17 -0300

    Remoção do Codigo JavaScript
    Organização do codigo e remoção do JavaScript

--- pages/head.php
+++ pages/head.php
@@ -3,13 +3,14 @@
 	<meta name="description" content="Cadastro de Pessoas para teste de conhecimentos em programação web">
 	<meta name="author" content="Eric Castro">
 
-
 	<title>Pentaxial Cadastro de Pessoas</title>
 
-	<link rel="icon" href="../img/favicon.ico">	
+	<link rel="icon" href="img/favicon.ico">	
 	<!-- CSS do Bootstrap -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="css/pagination.css">
+	<link rel="stylesheet" type="text/css" href="css/offcanvas.css">
+
 	<style type="text/css">
 		// Dispositivos extra small (telefones em modo retrato, com menos de 576px)
 		// Sem media query para `xs`, já que este é o padrão, no Bootstrap.
@@ -26,8 +27,3 @@
 		// Dispositivos extra large (desktops grandes com 1200px ou mais)
 		@media (min-width: 1200px) { ... }
 	</style>
-
-	<!-- Primeiro o jQuery, depois o Popper.js, e depois o Bootstrap JS -->
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
