b8b219496cacb0901b70fc9a80a5fd9a38ae663f
Eric Oliveira de Castro <eric.castro.dev@gmail.com>
2019-08-29 10:33:59 -0300

    Create head.php
    inclusao da pagina de <meta tags>

---
+++ pages/head.php
@@ -0,0 +1,31 @@
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="Cadastro de Pessoas para teste de conhecimentos em programação web">
+
+	<link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.css">
+
+	<title>Pentaxial Cadastro de Pessoas</title>
+	<!-- CSS do Bootstrap -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<style type="text/css">
+		// Dispositivos extra small (telefones em modo retrato, com menos de 576px)
+		// Sem media query para `xs`, já que este é o padrão, no Bootstrap.
+
+		// Dispositivos small (telefones em modo paisagem, com 576px ou mais)
+		@media (min-width: 576px) { ... }
+
+		// Dispositivos médios (tablets com 768px ou mais)
+		@media (min-width: 768px) { ... }
+
+		// Dispositivos large (desktops com 992px ou mais)
+		@media (min-width: 992px) { ... }
+
+		// Dispositivos extra large (desktops grandes com 1200px ou mais)
+		@media (min-width: 1200px) { ... }
+	</style>
+
+	<!-- Primeiro o jQuery, depois o Popper.js, e depois o Bootstrap JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.15.0/esm/popper.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
