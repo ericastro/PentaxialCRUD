22ebfd56a40f761370e4aa40770dafd1a15c248b
Eric Oliveira de Castro <eric.castro.dev@gmail.com>
2019-08-29 20:32:42 -0300

    Create create.php

---
+++ create.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+	<?php include_once 'pages/head.php'; ?>
+</head>
+
+<body>
+	<!-- Menu Navegação -->
+	<section>
+		<nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Alterna navegação">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<a class="navbar-brand" href="#">
+				<img src="img/logo-pentaxial-10-anos.svg" class="d-inline-block align-top" height="30" alt="Logo Pentaxial 10 anos">
+			</a>
+
+			<div class="collapse navbar-collapse" id="navbarNavDropdown">
+				<ul class="navbar-nav">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Home <span class="sr-only">(Página atual)</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Destaques</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Preços</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</section>
+	<!-- Menu Navegação -->
+
+	<!-- Container Principal -->
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<h1>Cadastro de Pessoas</h1>					
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12">
+
+			</div>
+		</div>
+	</div>
+	<!-- Container Principal -->
+
+	<footer class="fixed-bottom bg-dark" style="color: white">
+		<p class="text-right wh-100">Developer by Eric Castro</p>
+	</footer>
+
+</body>
+<?php include_once 'pages/javascript.php'; ?>
+</html>
