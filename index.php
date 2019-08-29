bc50f57f16059f726a015a54a47c798bbad1d55c
Eric Oliveira de Castro <eric.castro.dev@gmail.com>
2019-08-29 18:05:57 -0300

    Menu de Navegação
    inclusão do menu de navegação

--- index.php
+++ index.php
@@ -6,6 +6,30 @@
 </head>
 
 <body>
+	<div class="container">
+		<nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Alterna navegação">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<a class="navbar-brand" href="#">
+				<img src="img/logo-pentaxial-10-anos.svg" class="d-inline-block align-top" height="30" alt="Logo Pentaxial 10 anos">
+			</a>
+
+			<div class="collapse navbar-collapse" id="navbarNavDropdown">
+				<ul class="navbar-nav">
+					<li class="nav-item active">
+						<a class="nav-link" href="#">Home <span class="sr-only">(Página atual)</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Destaques</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Preços</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</div>
 </body>
 
 </html>
