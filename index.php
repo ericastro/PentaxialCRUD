fd09462416708c97fa889497efb10740cfff7dda
Eric Oliveira de Castro <eric.castro.dev@gmail.com>
2019-08-30 07:37:33 -0300

    Update index.php
    função que apaga e mostra os dados que restaram

--- index.php
+++ index.php
@@ -5,13 +5,22 @@
 	$crud 		= new CRUD();
 	$table 		= "pessoas";
 
-	try
+	if ( empty($_GET) )
 	{
-		$pessoas = $crud->Read($table,"","idPessoa ASC");
+		try
+		{
+			$pessoas = $crud->Read($table,"","idPessoa ASC","*");
+		}
+		catch (Exeption $ex)
+		{
+			$error = "Ocorreu o erro : " + $ex.message();
+		}
 	}
-	catch (Exeption $ex)
+	else
 	{
-		$error = "Ocorreu o erro : " + $ex.message();
+		$pessoas = $crud->Delete($table,$_GET['apagarId']);
+		
+		$pessoas = $crud->Read($table,"","idPessoa ASC","*");
 	}
 ?>
 
@@ -37,6 +46,9 @@
 			</div>
 		</div>
 		<div class="row">
+			<div class="col-12"><p>Clique nos dados da pessoa para editar os dados ou Excluir.</p></div>
+		</div>
+		<div class="row">
 			<div class="col-12">
 				<table class="table table-striped table-hover">
 					<thead class="thead-dark">
@@ -51,7 +63,8 @@
 						<?php
 							if(is_object($pessoas))
 							{
-								foreach ($pessoas as $pessoa) {
+								foreach ($pessoas as $pessoa) 
+								{
 									echo '<tr onclick="editarPessoa(' . $pessoa->idPessoa . ')">';
 									echo '<th scope="row">' . $pessoa->idPessoa . '</th>';
 									echo '<td>' . $pessoa->nome . '</td>';
