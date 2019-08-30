48f765f5d983559e6a2b63ca935138f8078fc21e
Eric Oliveira de Castro <eric.castro.dev@gmail.com>
2019-08-30 03:47:08 -0300

    Update index.php
    Organização na Barra de Navegação e correção de pequenos erros

--- index.php
+++ index.php
@@ -8,28 +8,7 @@
 <body>
 	<!-- Menu Navegação -->
 	<section>
-		<nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Alterna navegação">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<a class="navbar-brand" href="#">
-				<img src="img/logo-pentaxial-10-anos.svg" class="d-inline-block align-top" height="30" alt="Logo Pentaxial 10 anos">
-			</a>
-
-			<div class="collapse navbar-collapse" id="navbarNavDropdown">
-				<ul class="navbar-nav">
-					<li class="nav-item active">
-						<a class="nav-link" href="#">Home <span class="sr-only">(Página atual)</span></a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Destaques</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Preços</a>
-					</li>
-				</ul>
-			</div>
-		</nav>
+		<?php include_once 'pages/nav.php'; ?>
 	</section>
 	<!-- Menu Navegação -->
 
@@ -38,7 +17,6 @@
 		<div class="row">
 			<div class="col-12">
 				<h1>Cadastro de Pessoas<a href="create.php" class="btn btn-secondary btn-dark btn-lg active ml-3" role="button" aria-pressed="true">+</a></h1>
-
 			</div>
 		</div>
 		<div class="row">
@@ -53,9 +31,9 @@
 						</tr>
 					</thead>
 					<tbody>
-						<tr onclick="alert();">
+						<tr onclick="editarPessoa('1');">
 							<th scope="row">1</th>
-							<td >Mark</td>
+							<td>Mark</td>
 							<td>Otto</td>
 							<td>@mdo</td>
 						</tr>
@@ -78,8 +56,15 @@
 	</div>
 	<!-- Container Principal -->
 
+	<script type="text/javascript">
+		function editarPessoa(idPessoa) {
+			alert(idPessoa);
+		}
+		$( document ).ready(function() {});
+	</script>
+
 	<footer class="fixed-bottom bg-dark footer-color">
-		<p class="text-right wh-100">Developer by Eric Castro</p>
+		<p class="text-right wh-100">Developed by Eric Castro</p>
 	</footer>
 
 </body>
