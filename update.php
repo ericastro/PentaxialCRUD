7fc044944b6dfd326d4890b1ee36ecf67cec864e
Eric Oliveira de Castro <eric.castro.dev@gmail.com>
2019-08-30 00:30:12 -0300

    Update update.php
    Organizando Menu de navegação

--- update.php
+++ update.php
@@ -8,28 +8,7 @@
 <body>
 	<!-- Menu Navegação -->
 	<section>
-		<nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Alterna navegação">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-			<a class="navbar-brand" href="#">
-				<img src="img/logo-pentaxial-10-anos.svg" class="d-inline-block align-top" height="30" alt="Logo Pentaxial 10 anos">
-			</a>
-
-			<div class="collapse navbar-collapse" id="navbarNavDropdown">
-				<ul class="navbar-nav">
-					<li class="nav-item active">
-						<a class="nav-link" href="#">Home <span class="sr-only">(Página atual)</span></a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Destaques</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Preços</a>
-					</li>
-				</ul>
-			</div>
-		</nav>
+		<?php include_once 'pages/nav.php'; ?>
 	</section>
 	<!-- Menu Navegação -->
 
@@ -41,7 +20,6 @@
 			</div>
 			<div class="col-sm-12 col-md-7">
 					<button class="btn-dark btn-lg ml-3" onclick="Editar();">Editar</button>
-					<button class="btn-danger btn-lg ml-3" onclick="Excluir('1')">Excluir</button>
 			</div>
 		</div>
 		<div class="row">
@@ -93,10 +71,6 @@
 			$( "#email" ).prop( "disabled", false );
 			$( "#files" ).prop( "disabled", false );
 			$( "#btnSalvar" ).prop( "disabled", false );
-		}
-
-		function Excluir(idPessoa){
-			alert(idPessoa);
 		}
 	</script>
 
